f10d516444ae743ea62d51bc645c1dd3a42dc2c4
Gilberto Espinoso <gilbertosilvaespinosa@gmail.com>
2024-02-26 22:19:05 -0300

    App.css > NavBar CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import "./App.css";
+import { NavBar } from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      
+      <NavBar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
